1131cd390cf21f125ddf8c1ad78be73cc3f9ed1d
Leave <Leave@atguigu.com>
2023-01-29 20:17:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chenqistart
+ * @create 2023-01-29 20:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
