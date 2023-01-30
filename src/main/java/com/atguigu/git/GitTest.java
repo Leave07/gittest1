41b3c02d05b21afffe1750108ae9097aba5e7074
Leave <Leave@atguigu.com>
2023-01-30 09:58:30 +0800

    four commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello gitHub");
         System.out.println("hello world");
-        System.out.println("hello 1");
+        System.out.println("master test");
     }
 }
